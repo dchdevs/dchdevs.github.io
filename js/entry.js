823bcb2b95dbcc2981c208169683fed5e8013200
Atul Singh <atulpratapsingh05@gmail.com>
2021-11-09 11:18:46 +0530

    Fix bug in totaling of columns

--- js/entry.js
+++ js/entry.js
@@ -111,7 +111,7 @@ function create_table() {
         }
         for (var i = 0; i < ob_shovels_operating.length; i++) {
             if(ob_shovels_operating[i] && ob_shovel_operator[i]) {
-                $(tr).append("<td><input name='" + ob_shovels_operating[i] + "_OB_" + ob_shovel_operator[i] + "[]' class='inp " + "sum" + (i + coal_shovels_operating.length + 4) + "' required='required' maxlength='128' type='number' value='' min='0' data-rule-required='true' data-msg-required='Please enter a valid number'></td>");
+                $(tr).append("<td><input name='" + ob_shovels_operating[i] + "_OB_" + ob_shovel_operator[i] + "[]' class='inp " + "sum" + (i + coal_shovels_operating.length + 5) + "' required='required' maxlength='128' type='number' value='' min='0' data-rule-required='true' data-msg-required='Please enter a valid number'></td>");
                 totals_html += '<td>0</td>';
             }
             if(i === ob_shovels_operating.length - 1) {
