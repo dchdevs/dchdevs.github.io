613ea9b764f297555c8e8843c02cf4bab613c914
Atul Singh <atulpratapsingh05@gmail.com>
2022-05-01 22:15:56 +0530

    New process orders for May

--- js/entry.js
+++ js/entry.js
@@ -11,9 +11,9 @@ var coal_shovel_seam = {};
 var ob_shovel_seam = {};
 var material_code_coal = 4900000014;
 var material_code_ob = 4900000011;
-var process_order_purewa_coal = 60008065;
-var process_order_turra_coal = 60008066;
-var process_order_ob = 70005488;
+var process_order_purewa_coal = 60008412;
+var process_order_turra_coal = 60008413;
+var process_order_ob = 70005667;
 //JSON for "the data"
 var dataForPage = [
     {
