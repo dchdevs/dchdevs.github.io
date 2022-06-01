e8d0dfab3be21538c317e990f93444f2d0b441e1
Dudhichua Devs <100851453+dchdevs@users.noreply.github.com>
2022-06-01 18:43:24 +0530

    New process orders for June

--- js/entry.js
+++ js/entry.js
@@ -11,9 +11,10 @@ var coal_shovel_seam = {};
 var ob_shovel_seam = {};
 var material_code_coal = 4900000014;
 var material_code_ob = 4900000011;
-var process_order_purewa_coal = 60008412;
-var process_order_turra_coal = 60008413;
-var process_order_ob = 70005667;
+var process_order_purewa_coal = 60009051;
+var process_order_turra_coal = 60009052;
+var process_order_ob = 70005997;
+
 //JSON for "the data"
 var dataForPage = [
     {
