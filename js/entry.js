6ab1910de19af74925725c8f810615577a9b0d1a
Atul Singh <atulpratapsingh05@gmail.com>
2022-08-01 12:58:52 +0530

    August Process Orders

--- js/entry.js
+++ js/entry.js
@@ -11,9 +11,9 @@ var coal_shovel_seam = {};
 var ob_shovel_seam = {};
 var material_code_coal = 4900000014;
 var material_code_ob = 4900000011;
-var process_order_purewa_coal = 60010241;
-var process_order_turra_coal = 60010242;
-var process_order_ob = 70006679;
+var process_order_purewa_coal = 60010855;
+var process_order_turra_coal = 60010856;
+var process_order_ob = 70007183;
 
 //JSON for "the data"
 var dataForPage = [
